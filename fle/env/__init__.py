10545941bcc1e5991165686054119e00f6c463d5
Neel Kant <kantneel@gmail.com>
2025-07-07 22:52:19 +0200

    more linting, exclude E722

--- fle/env/__init__.py
+++ fle/env/__init__.py
@@ -1,6 +1,6 @@
 """Environment and game interaction"""
 
-from .entities import *
+from .entities import Direction, Entity, Position, Inventory, EntityGroup
 from .game_types import Prototype, Resource
 from .instance import DirectionInternal, FactorioInstance
 

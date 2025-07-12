6dcb2836984a58791aa2e53d129897fed855ea8c
Kian Kyars <kiankyars@gmail.com>
2025-07-12 14:48:39 +0000

    Jul 12, 2025 at 17:48

--- fle/env/lua_manager.py
+++ fle/env/lua_manager.py
@@ -49,28 +49,6 @@ class LuaScriptManager:
                     return True, None
             return False, e.args[0]
 
-    # @deprecated("Using tools")
-    # def load_action_into_game(self, name):
-    #
-    #     if name not in self.action_scripts:
-    #         # attempt to load the script from the filesystem
-    #         script = _load_action(name)
-    #         self.action_scripts[name] = script
-    #
-    #     script = self.action_scripts[name]
-    #     if self.cache_scripts:
-    #         checksum = self.calculate_checksum(script)
-    #         if name in self.game_checksums and self.game_checksums[name] == checksum:
-    #             return
-    #         self.update_game_checksum(self.rcon_client, name, checksum)
-    #
-    #     correct, error = self.check_lua_syntax(script)
-    #     if not correct:
-    #         raise Exception(f"Syntax error in: {name}: {error}")
-    #     print(f"{self.rcon_client.port}: Loading action {name} into game")
-    #
-    #     result = self.rcon_client.send_command(f'/sc ' + script)
-
     def load_tool_into_game(self, name):
         # Find all scripts for this action by checking prefixes
         tool_scripts = [
@@ -129,22 +107,6 @@ class LuaScriptManager:
 
     def calculate_checksum(self, content: str) -> str:
         return hashlib.md5(content.encode()).hexdigest()
-
-    # @deprecated("Moving to tools")
-    # def get_actions_to_load(self):
-    #     scripts_to_load = {}
-    #     script_names = _get_action_names()
-    #     for script_file in script_names:
-    #         name, content = _load_script(script_file)
-    #
-    #         if self.cache_scripts:
-    #             checksum = self.calculate_checksum(content)
-    #             if (name not in self.game_checksums or
-    #                 self.game_checksums[name] != checksum):
-    #                 scripts_to_load[name] = content
-    #         else:
-    #             scripts_to_load[name] = content
-    #     return scripts_to_load
 
     def get_tools_to_load(self):
         scripts_to_load = {}

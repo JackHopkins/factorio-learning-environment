2370d3aa47f8471f3aa1149aa6889658a98b0db4
Kian Kyars <kiankyars@gmail.com>
2025-08-26 09:06:16 +0000

    original

--- fle/__init__.py
+++ fle/__init__.py
@@ -4,14 +4,3 @@
 from fle import agents, env, eval, cluster, commons
 
 __all__ = ["agents", "env", "eval", "cluster", "commons"]
-
-# Ensure Ctrl-C exits cleanly across OSes without stack traces
-import signal
-import sys
-
-
-def _exit_on_sigint(signum, frame):
-    sys.exit(0)
-
-
-signal.signal(signal.SIGINT, _exit_on_sigint)
